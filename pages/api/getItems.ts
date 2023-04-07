54734a5a7a1650f711ea24cb860b3b92ce95e270
Mopsior <bartek.szg@gmail.com>
2023-04-07 12:54:50 +0200

    api fix

--- pages/api/getItems.ts
+++ pages/api/getItems.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { promises as fs } from 'fs';
+import path from 'path';
 
 type Data = {
   name: string
@@ -9,6 +10,7 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<Data>
 ) {
-    const fileContents = await fs.readFile(process.cwd() + process.env.NEXT_PUBLIC_JSON_DIRECTORY, 'utf8');
+    const jsonDirectory = path.join(process.cwd(), 'public');
+    const fileContents = await fs.readFile(jsonDirectory + process.env.NEXT_PUBLIC_JSON_DIRECTORY, 'utf8');
     res.status(200).json(JSON.parse(fileContents))
 }
