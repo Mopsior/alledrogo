0a7a5af9c6ef9c67b5bee968e4203389ef6bf077
Mopsior <bartek.szg@gmail.com>
2023-04-09 15:01:49 +0200

    Add Hamburger menu blur

--- pages/kontakt/index.tsx
+++ pages/kontakt/index.tsx
@@ -1,9 +1,12 @@
+import { HamburgerContext } from "@/components/Navbar/Hamburger/HamburgerContext";
 import { ContactContent } from "@/features/Contact/ContactContent/ContactContent";
 import Head from "next/head";
+import { useContext } from "react";
 
 export default function ContactPage() {
+    const { isOpen } = useContext(HamburgerContext);
     return (
-        <>
+        <div className={isOpen ? 'blur' : ''}>
             <Head>
                 <title>Alledrogo - kontakt</title>
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -11,6 +14,6 @@ export default function ContactPage() {
                 <meta name="description" content="Skontaktuj się z nami" />
             </Head>
             <ContactContent />
-        </>
+        </div>
     )
 }
