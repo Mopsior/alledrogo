ae8997eca09757f645629ea002a49a9294b4bae0
Mopsior <bartek.szg@gmail.com>
2023-04-09 15:18:47 +0200

    Trying to fix Vercel

--- pages/api/getItems.ts
+++ pages/api/getItems.ts
@@ -3,15 +3,17 @@ import { promises as fs } from 'fs';
 import path from 'path';
 
 type Data = {
-  name: string
+    name: string
 }
 
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<Data>
 ) {
+    console.warn('process.cwd()', process.cwd())
+    console.warn('process.env.NEXT_PUBLIC_JSON_DIRECTORY', process.env.NEXT_PUBLIC_JSON_DIRECTORY)
     const jsonDirectory = path.join(process.cwd(), 'public');
-    console.log('123456', process.cwd(), jsonDirectory, process.env.NEXT_PUBLIC_JSON_DIRECTORY)
+    console.log("🚀 ~ file: getItems.ts:16 ~ jsonDirectory:", jsonDirectory)
     const fileContents = await fs.readFile(jsonDirectory + process.env.NEXT_PUBLIC_JSON_DIRECTORY, 'utf8');
     res.status(200).json(JSON.parse(fileContents))
 }
